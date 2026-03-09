37bb06b15e425e33d2d26ceb72a9fe38ecb5cb19
Romain Plumaugat <romain.plumaugat@protonmail.com>
2026-03-09 13:32:39 +0100

    Add file exists check in readSpirV function

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,9 +1,16 @@
 #include "Utils.hpp"
 
 #include <fstream>
+#include "Logger.hpp"
 
 std::vector<char> readSpirV(const std::string &path) {
     std::ifstream file(path, std::ios::ate | std::ios::binary);
+
+    if (!file) {
+        LOG_ERROR("\'{}\': File not found.", path);
+        return std::vector<char>();
+    }
+
     size_t size = file.tellg();
     std::vector<char> buf(size);
     file.seekg(0);
