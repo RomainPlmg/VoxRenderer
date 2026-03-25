a948e3d543a57426310659c57a14c2c0f02366a3
Romain Plumaugat <romain.plumaugat@protonmail.com>
2026-03-25 10:38:54 +0100

    Add class for GPU buffers

---
+++ include/Buffer.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <vk_mem_alloc.h>
+#include "VkContext.hpp"
+
+class UniformBuffer {
+public:
+    UniformBuffer(VkDevice device, VmaAllocator allocator, size_t size);
+    ~UniformBuffer();
+
+    template<typename T>
+    void emplace(T *data, uint32_t size, uint32_t offset = 0);
+    void destroy();
+
+    /* Getters */
+    VkDeviceAddress address() const { return m_address; }
+
+private:
+    VkBuffer m_buffer = VK_NULL_HANDLE;
+    VmaAllocation m_allocation = VK_NULL_HANDLE;
+    VmaAllocationInfo m_allocInfo{};
+    VkDeviceAddress m_address = 0;
+
+    VmaAllocator m_allocator;
+};
+
+template<typename T>
+void UniformBuffer::emplace(T *data, uint32_t size, uint32_t offset) {
+    auto *mapped = static_cast<uint8_t *>(m_allocInfo.pMappedData);
+    memcpy(mapped + offset, data, size);
+}
---
+++ src/Buffer.cpp
@@ -0,0 +1,33 @@
+#include "Buffer.hpp"
+
+UniformBuffer::UniformBuffer(VkDevice device, VmaAllocator allocator, size_t size) : m_allocator(allocator) {
+    VkBufferCreateInfo bufferInfo{
+            .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+            .size = size,
+            .usage = VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT | VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT,
+    };
+
+    VmaAllocationCreateInfo allocCreateInfo{
+            .flags = VMA_ALLOCATION_CREATE_MAPPED_BIT | VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT,
+            .usage = VMA_MEMORY_USAGE_AUTO,
+    };
+
+    VK_CHECK(vmaCreateBuffer(m_allocator, &bufferInfo, &allocCreateInfo, &m_buffer, &m_allocation, &m_allocInfo));
+
+    VkBufferDeviceAddressInfo addrInfo{
+            .sType = VK_STRUCTURE_TYPE_BUFFER_DEVICE_ADDRESS_INFO,
+            .buffer = m_buffer,
+    };
+
+    m_address = vkGetBufferDeviceAddress(device, &addrInfo);
+}
+
+UniformBuffer::~UniformBuffer() {
+    if (m_buffer != VK_NULL_HANDLE)
+        destroy();
+}
+
+void UniformBuffer::destroy() {
+    vmaDestroyBuffer(m_allocator, m_buffer, m_allocation);
+    m_buffer = VK_NULL_HANDLE;
+}
